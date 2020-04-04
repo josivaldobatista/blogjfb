bff4bf547f421b6e648a3a3f6ce62e72a7d2e517
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-04-04 17:32:27 -0300

    Primeiro commit, criação do projeto.

---
+++ src/main/java/com/jfb/blogjfb/BlogJfbApplication.java
@@ -0,0 +1,14 @@
+package com.jfb.blogjfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJfbApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BlogJfbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfb/blogjfb/BlogjfbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfb.blogjfb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogjfbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
