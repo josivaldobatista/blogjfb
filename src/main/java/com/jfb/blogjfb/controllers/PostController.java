f42d53adb75a4abeffe00e2260afa706d2203385
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-04-08 03:16:50 -0300

    Criando método e formulário para salvar um post.

--- src/main/java/com/jfb/blogjfb/controllers/PostController.java
+++ src/main/java/com/jfb/blogjfb/controllers/PostController.java
@@ -1,16 +1,22 @@
 package com.jfb.blogjfb.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import com.jfb.blogjfb.models.Post;
 import com.jfb.blogjfb.services.PostService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 @RequestMapping("/posts")
@@ -33,6 +39,23 @@ public class PostController {
         Post post = service.findById(id);
         mv.addObject("post", post);
         return mv;
+    }
+
+    @GetMapping("/newpost")
+    public ModelAndView getPostForm() {
+        ModelAndView mv = new ModelAndView("postForm");
+        return mv;
+    }
+
+    @PostMapping("/newpost")
+    public ModelAndView savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+        if (result.hasErrors()) {
+            // attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return new ModelAndView("redirect:/posts/newpost");
+        }
+        post.setData(LocalDate.now());
+        service.save(post);
+        return new ModelAndView("redirect:/posts");
     }
 
 }
